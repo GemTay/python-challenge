d7bdb9a24b2d07cce23949a71dbe21cf26b89717
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-01-11 14:04:10 +0000

    validates numbers to the correct pattern

--- validate_credit_card_numbers.py
+++ validate_credit_card_numbers.py
@@ -1,21 +1,38 @@
 import csv
+import re
 
 class ValidateCreditCardNumbers:
 
-    def __init__(self):
-        self.csv_as_array = []
-        file=open("credit_card_numbers.csv", "r")
-        reader = csv.reader(file)
-        for line in reader:
-            self.csv_as_array.append(line[0])
-
-        print(self.csv_as_array)
-        check_N_in_range(self.csv_as_array[0])
-
-    def check_N_in_range(n):
-        if n > 0 and n < 100:
+    def check_N_in_range(self, n):
+        self.n = int(n)
+        #if n is 0?
+        if self.n > 0 and self.n < 100:
             print("In range")
+            self.validate_credit_numbers(self.credit_numbers)
         else:
             print("Not in range")
 
-ValidateCreditCardNumbers()
+    def validate_credit_numbers(self, numbers_array):
+        for i in range(1, (self.n)+1):
+            if not int(numbers_array[i][:1]) in [4,5,6]:
+                print("Credit card number %s does not begin with a 4, 5 or 6" % numbers_array[i])
+
+            result = re.match(r"^(\d{4}-?){3}(\d{4})$", numbers_array[i])
+            if not result:
+                print("Credit card number %s is not in the correct format" % numbers_array[i])
+
+
+
+
+
+    def read_csv(self):
+        self.credit_numbers = []
+        file=open("credit_card_numbers.csv", "r")
+        reader = csv.reader(file)
+        for line in reader:
+            self.credit_numbers.append(line[0])
+
+        self.check_N_in_range(self.credit_numbers[0])
+
+if __name__ == '__main__':
+    ValidateCreditCardNumbers().read_csv()
